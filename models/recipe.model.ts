517fca1389c82ea3944716ab1bbe2c5ecb600e5b
adrienraynaudd <adrien48632@gmail.com>
2025-03-05 14:24:51 +0100

    add step to recipe model

--- models/recipe.model.ts
+++ models/recipe.model.ts
@@ -3,6 +3,7 @@ export interface Recipe {
     name: string,
     ingredientsId: string[],
     description: string,
+    step: string,
     categoriesId: string[],
     time: number,
     origin: string
