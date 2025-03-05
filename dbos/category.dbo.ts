d5ddc40b84489787d78ed9e451e4cb438bf98ad7
Adrienraynaudd <70946168+Adrienraynaudd@users.noreply.github.com>
2025-03-05 15:13:57 +0100

    dbos (#4)
    * dbos
    
    * Rename id property to _id in CategoryDBO class

---
+++ dbos/category.dbo.ts
@@ -0,0 +1,24 @@
+import { ObjectId } from "https://deno.land/x/mongo@v0.34.0/mod.ts";
+import { Category } from "../models/category.ts";
+
+export class CategoryDBO {
+    _id: ObjectId;
+    name: string;
+    Type: string;
+
+    constructor(id : string, name: string, Type: string) {
+        this.id = new ObjectId(id);
+        this.name = name;
+        this.Type = Type;
+    }
+    static fromCategory(category: Category): CategoryDBO {
+        return new CategoryDBO(category.id, category.name, category.Type);
+    }
+    static toCategory(categoryDBO: CategoryDBO): Category {
+        return {
+            id: categoryDBO.id.toString(),
+            name: categoryDBO.name,
+            Type: categoryDBO.Type,
+        };
+    }
+}
