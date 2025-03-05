5e886c20d4900b5f6c9082c2196b97301e15c14b
adrienraynaudd <adrien48632@gmail.com>
2025-03-05 20:46:00 +0100

    tri

--- repositories/recipe.repository.ts
+++ repositories/recipe.repository.ts
@@ -3,7 +3,12 @@ import { RecipeDBO } from "../dbos/recipe.dbo.ts";
 import { db } from "../db.ts";
 import { createHttpError } from "https://deno.land/x/oak@v17.1.4/deps.ts";
 import { ObjectId } from "https://deno.land/x/mongo@v0.34.0/mod.ts";
+
 export class RecipeRepository {
+
+    private mapRecipesFromDB(recipesDBO: RecipeDBO[]): Recipe[] {
+        return recipesDBO.map((recipeD: RecipeDBO) => RecipeDBO.toRecipe(recipeD));
+    }
 
     async getAllRecipes(): Promise<Recipe[]> {
         try {
@@ -11,8 +16,8 @@ export class RecipeRepository {
             if (recipes.length === 0) {
                 throw createHttpError(404, 'Aucune recette trouvée');
             }
-            return recipes.map((recipe :  RecipeDBO) => RecipeDBO.toRecipe(recipe));
-        } catch  {
+            return this.mapRecipesFromDB(recipes);
+        } catch {
             throw createHttpError(500, `Erreur lors de la récupération des recettes: `);
         }
     }
@@ -25,7 +30,7 @@ export class RecipeRepository {
                 throw createHttpError(404, `Recette avec l'ID ${id} non trouvée`);
             }
             return RecipeDBO.toRecipe(recipeD);
-        } catch  {
+        } catch {
             throw createHttpError(500, `Erreur lors de la récupération de la recette`);
         }
     }
@@ -36,46 +41,44 @@ export class RecipeRepository {
             const insertResult = await db.getRecipesCollection().insertOne(recipeD);
 
             if (!insertResult) {
-                throw createHttpError(500,'Échec de l\'insertion de la recette dans la base de données.');
+                throw createHttpError(500, 'Échec de l\'insertion de la recette dans la base de données.');
             }
             recipeD._id = insertResult;
             const newRecipe = RecipeDBO.toRecipe(recipeD);
             return newRecipe;
-        } catch  {
+        } catch {
             throw createHttpError(500, `Erreur lors de la création de la recette`);
         }
-}
-
-
- async updateRecipe(id: string, recipe: Recipe): Promise<Recipe> {
-    try {
-         const objectId = new ObjectId(id);
-         const recipeD = RecipeDBO.fromRecipe(recipe);
-
-         const updateResult = await db.getRecipesCollection().updateOne({ _id: objectId },{ $set: recipeD });
-         if (updateResult.matchedCount === 0) {
-             throw createHttpError(404, `Recette avec l'ID ${id} non trouvée`);
-         }
-         recipeD._id = objectId;
-         const newRecipe = RecipeDBO.toRecipe(recipeD);
-         return newRecipe;
-     } catch  {
-         throw createHttpError(500, `Erreur lors de la mise à jour de la recette`);
-     }
- }
-
-
-async deleteRecipe(id: string): Promise<void> {
-    try {
-        const objectId = new ObjectId(id);
-        const deleteResult = await db.getRecipesCollection().deleteOne({ _id: objectId });
-        if (deleteResult.deletedCount === 0) {
-            throw createHttpError(404, `Recette avec l'ID ${id} non trouvée`);
-        }
-    } catch  {
-        throw createHttpError(500, `Erreur lors de la suppression de la recette`);
     }
-}
+
+    async updateRecipe(id: string, recipe: Recipe): Promise<Recipe> {
+        try {
+            const objectId = new ObjectId(id);
+            const recipeD = RecipeDBO.fromRecipe(recipe);
+
+            const updateResult = await db.getRecipesCollection().updateOne({ _id: objectId }, { $set: recipeD });
+            if (updateResult.matchedCount === 0) {
+                throw createHttpError(404, `Recette avec l'ID ${id} non trouvée`);
+            }
+            recipeD._id = objectId;
+            const newRecipe = RecipeDBO.toRecipe(recipeD);
+            return newRecipe;
+        } catch {
+            throw createHttpError(500, `Erreur lors de la mise à jour de la recette`);
+        }
+    }
+
+    async deleteRecipe(id: string): Promise<void> {
+        try {
+            const objectId = new ObjectId(id);
+            const deleteResult = await db.getRecipesCollection().deleteOne({ _id: objectId });
+            if (deleteResult.deletedCount === 0) {
+                throw createHttpError(404, `Recette avec l'ID ${id} non trouvée`);
+            }
+        } catch {
+            throw createHttpError(500, `Erreur lors de la suppression de la recette`);
+        }
+    }
 
     async searchRecipesByName(name: string): Promise<Recipe[]> {
         try {
@@ -83,8 +86,8 @@ async deleteRecipe(id: string): Promise<void> {
             if (recipes.length === 0) {
                 throw createHttpError(404, `Aucune recette trouvée avec le nom '${name}'`);
             }
-            return recipes.map((recipeD :  RecipeDBO) => RecipeDBO.toRecipe(recipeD));
-        } catch  {
+            return this.mapRecipesFromDB(recipes);
+        } catch {
             throw createHttpError(500, `Erreur lors de la recherche de recettes`);
         }
     }
@@ -96,8 +99,8 @@ async deleteRecipe(id: string): Promise<void> {
             if (recipes.length === 0) {
                 throw createHttpError(404, `Aucune recette trouvée avec l'ingrédient ID '${ingredientId}'`);
             }
-            return recipes.map((recipeD :  RecipeDBO) => RecipeDBO.toRecipe(recipeD));
-        } catch  {
+            return this.mapRecipesFromDB(recipes);
+        } catch {
             throw createHttpError(500, `Erreur lors de la recherche de recettes`);
         }
     }
@@ -105,25 +108,24 @@ async deleteRecipe(id: string): Promise<void> {
     async searchRecipesByCategories(categoryIds: string[]): Promise<Recipe[]> {
         try {
             const objectIds = categoryIds.map(id => new ObjectId(id));
-            const recipesD = await db.getRecipesCollection().find({ categoriesId: { $in: objectIds } }) .toArray();
+            const recipesD = await db.getRecipesCollection().find({ categoriesId: { $in: objectIds } }).toArray();
 
             if (recipesD.length === 0) {
                 throw createHttpError(404, `Aucune recette trouvée pour les catégories ID '${categoryIds.join(", ")}'`);
             }
-            return recipesD.map((recipeD : RecipeDBO) => RecipeDBO.toRecipe(recipeD));
-        } catch  {
+            return this.mapRecipesFromDB(recipesD);
+        } catch {
             throw createHttpError(500, `Erreur lors de la recherche de recettes`);
         }
     }
-    
 
     async searchRecipesByTime(maxTime: number): Promise<Recipe[]> {
-        try{
+        try {
             const recipes = await db.getRecipesCollection().find({ time: { $lte: maxTime } }).toArray();
             if (recipes.length === 0) {
                 throw createHttpError(404, `Aucune recette trouvée pour un temps inférieur à ${maxTime} minutes`);
             }
-            return recipes.map((recipeD :  RecipeDBO) => RecipeDBO.toRecipe(recipeD));
+            return this.mapRecipesFromDB(recipes);
         } catch {
             throw createHttpError(500, `Erreur lors de la recherche de recettes`);
         }
