061898bf2a57e9715496e799d03e5bea9c71df65
Adrienraynaudd <70946168+Adrienraynaudd@users.noreply.github.com>
2025-03-27 10:52:48 +0100

    Back/categories filter (#28)
    * v1
    
    * v1
    
    * v1
    
    * v1

---
+++ Back/dtos/category/filter-category.dto.ts
@@ -0,0 +1,17 @@
+import { ErrorObject } from "../../models/error.model.ts";
+
+export class FilterCategoryDTO {
+    constructor(
+        public type: string | null
+    ) {}
+
+    public static fromRequest(data: any): FilterCategoryDTO {
+        return new FilterCategoryDTO(data.type || null);
+    }
+
+    public validate(): void {
+        if (this.type !== null && typeof this.type !== "string") {
+            throw new ErrorObject('Bad Request', `Le type de catégorie doit être une chaîne de caractères valide.`);
+        }
+    }
+}
--- Back/services/category.service.ts
+++ Back/services/category.service.ts
@@ -3,43 +3,44 @@ import { CategoryDTO } from "../dtos/category/category.dto.ts";
 import { UpdateCategoryDTO } from "../dtos/category/update-category.dto.ts";
 import { Category } from "../models/category.model.ts";
 import { CategoryRepository } from "../repositories/category.repository.ts";
+import { FilterCategoryDTO } from "../dtos/category/filter-category.dto.ts";
 
 export class CategoryService {
-
     private readonly categoryRepository: CategoryRepository = new CategoryRepository();
 
-    async getAllCategories(): Promise<CategoryDTO[]> {
-        const categories = await  this.categoryRepository.getAllCategories()
-        return categories.map(category => CategoryDTO.fromModel(category))
+    async getAllCategories(filter?: FilterCategoryDTO): Promise<CategoryDTO[]> {
+        const type = filter?.type ?? undefined;
+        const categories = await this.categoryRepository.getAllCategories(type);
+        return categories.map(category => CategoryDTO.fromModel(category));
     }
 
     async getCategoryById(id: string): Promise<CategoryDTO> {
-        const category = await this.categoryRepository.getCategoryById(id)
-        return CategoryDTO.fromModel(category)
+        const category = await this.categoryRepository.getCategoryById(id);
+        return CategoryDTO.fromModel(category);
     }
 
-    async createCategory(categoryTdo: AddCategoryDTO): Promise<CategoryDTO> {
+    async createCategory(categoryDto: AddCategoryDTO): Promise<CategoryDTO> {
         let category: Category = {
             id: '',
-            name: categoryTdo.name,
-            Type: categoryTdo.Type
-        }
-        category = await this.categoryRepository.createCategory(category)
-        return CategoryDTO.fromModel(category)
+            name: categoryDto.name,
+            Type: categoryDto.Type
+        };
+        category = await this.categoryRepository.createCategory(category);
+        return CategoryDTO.fromModel(category);
     }
 
-    async updateCategory(categoryTdo: UpdateCategoryDTO): Promise<CategoryDTO> {
-        let category = await this.categoryRepository.getCategoryById(categoryTdo.id)
+    async updateCategory(categoryDto: UpdateCategoryDTO): Promise<CategoryDTO> {
+        let category = await this.categoryRepository.getCategoryById(categoryDto.id);
         category = {
-            id: categoryTdo.id,
-            name: categoryTdo.name || category.name,
-            Type: categoryTdo.Type || category.Type
-        }
-        category = await this.categoryRepository.updateCategory(category.id, category)
-        return CategoryDTO.fromModel(category)
+            id: categoryDto.id,
+            name: categoryDto.name || category.name,
+            Type: categoryDto.Type || category.Type
+        };
+        category = await this.categoryRepository.updateCategory(category.id, category);
+        return CategoryDTO.fromModel(category);
     }
 
     async deleteCategory(id: string): Promise<void> {
-        await this.categoryRepository.deleteCategory(id)
+        await this.categoryRepository.deleteCategory(id);
     }
 }
