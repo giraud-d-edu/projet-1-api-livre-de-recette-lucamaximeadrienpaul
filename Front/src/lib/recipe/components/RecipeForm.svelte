ca7abf1da0f3edcf2f8c464f307be9a78ae49d8d
adrienraynaudd <adrien48632@gmail.com>
2025-03-27 11:20:35 +0100

    v3

--- Front/src/lib/recipe/components/RecipeForm.svelte
+++ Front/src/lib/recipe/components/RecipeForm.svelte
@@ -19,6 +19,11 @@
 
 	export let submit: (recipe: UpdateRecipe | AddRecipe) => void;
 
+	onMount(() => {
+		ingredientStore.load();
+		categoryStore.load();
+	});
+
 	function handleFile(event: Event) {
 		const target = event.target as HTMLInputElement;
 		if (target.files && target.files.length > 0) {
@@ -28,30 +33,37 @@
 </script>
 
 <form on:submit|preventDefault={() => submit(recipe)}>
-	<label>
-		Nom de la recette
-		<input type="text" bind:value={recipe.name} />
-	</label>
-	<label>
-		Temps de préparation
-		<input type="number" bind:value={recipe.time} />
-	</label>
-	<label>
-		Description
-		<textarea bind:value={recipe.description}></textarea>
-	</label>
-	<label>
-		Origine
-		<input type="text" bind:value={recipe.origin} />
-	</label>
-	<label>
-		Étapes
-		<textarea bind:value={recipe.step}></textarea>
-	</label>
-	<label>
-		Image
-		<input type="file" accept="image/*" on:change={handleFile} />
-	</label>
+	<label for="name">Name:</label>
+	<input id="name" bind:value={recipe.name} />
+
+	<label for="ingredients">Ingredients:</label>
+	<select id="ingredients" bind:value={recipe.ingredientsId} multiple>
+		{#each $ingredients as ingredient}
+			<option value={ingredient.id}>{ingredient.name}</option>
+		{/each}
+	</select>
+
+	<label for="categories">Categories:</label>
+	<select id="categories" bind:value={recipe.categoriesId} multiple>
+		{#each $categories as category}
+			<option value={category.id}>{category.name}</option>
+		{/each}
+	</select>
+
+	<label for="description">Description:</label>
+	<textarea id="description" bind:value={recipe.description}></textarea>
+
+	<label for="step">Step:</label>
+	<textarea id="step" bind:value={recipe.step}></textarea>
+
+	<label for="time">Time:</label>
+	<input id="time" type="number" bind:value={recipe.time} />
+
+	<label for="origin">Origin:</label>
+	<input id="origin" bind:value={recipe.origin} />
+
+	<label for="image">Image:</label>
+	<input type="file" accept="image/*" on:change={handleFile} />
 
 	{#if recipe.image}
 		<img src={URL.createObjectURL(recipe.image)} alt="" width="200" />
