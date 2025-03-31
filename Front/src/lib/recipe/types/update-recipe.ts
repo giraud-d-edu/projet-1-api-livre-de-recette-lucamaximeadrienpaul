1e0efad864feaee5f757b7a596f03201ee90d514
MaxChevalier <maxime.chevalier@ynov.com>
2025-03-31 18:51:58 +0200

    fix image update

--- Front/src/lib/recipe/types/update-recipe.ts
+++ Front/src/lib/recipe/types/update-recipe.ts
@@ -1,11 +1,11 @@
 export type UpdateRecipe = {
     id: string,
-    name:string,
-    ingredientsId:string[],
-    description:string,
-    step:string,
-    categoriesId:string[],
-    time:number,
-    origin:string,
-    image:File|null
+    name: string,
+    ingredientsId: string[],
+    description: string,
+    step: string,
+    categoriesId: string[],
+    time: number,
+    origin: string,
+    image: File | string | null
 }
